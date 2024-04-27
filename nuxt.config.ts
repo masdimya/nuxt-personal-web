cecd685953ab4f9fe2a688d67fdfff345465e2fc
dimas.aulia <dimas.aulia@elnusa.co.id>
2024-04-27 20:29:37 +0700

    add nuxtjs/google-fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content'],
+  modules: [
+    '@nuxt/content',
+    '@nuxtjs/google-fonts'
+  ],
+  googleFonts: {
+    /* module options */
+    families: {
+      'IBM+Plex+Mono': true, // Enable IBM Plex Mono font
+    },
+    display: 'swap', // Use font-display with swap
+    preconnect: true, // Preconnect to Google Fonts
+    download: true, // Download fonts automatically
+    inject: true, // Inject the font links in the head of the document
+  },
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   postcss: {
